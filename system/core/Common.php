a9748d733b4f0f4f4f3d23ad5e6914546b62c3ea
adamGuan <ghq1986@gmail.com>
2015-03-06 14:00:25 +0800

    for php5.6

--- system/core/Common.php
+++ system/core/Common.php
@@ -253,8 +253,10 @@ if ( ! function_exists('get_config'))
 				}
 			}
 		}
-
-		return $_config[0] =& $config;
+//		return $_config[0] =& $config;
+		//adam modify
+		$_config[0] = & $config;
+		return $_config[0];
 	}
 }
 
